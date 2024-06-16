d0bdd5835a2a4936cf9e8d2f636d33163997f3d3
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-06-16 16:14:54 +0530

    commit

---
+++ QuestionsFRomPlatforms/Leetcode/CountPrimes_204.java
@@ -0,0 +1,16 @@
+package QuestionsFromPlatforms.Leetcode;
+import java.util.Scanner;
+public class CountPrimes_204 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int counter = 0;
+        if(n == 1|| n==0){
+            System.out.println(0);
+        }
+        for(int i =2 ; i< n ; i++){
+            i%
+        }
+
+    }
+}
---
+++ QuestionsFRomPlatforms/Leetcode/UglyNumber_263.java
@@ -0,0 +1,8 @@
+package QuestionsFromPlatforms.Leetcode;
+
+public class UglyNumber_263 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
