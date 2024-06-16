5d9699698e49e73997734f58bd559d1dfe4467e6
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-06-17 00:27:01 +0530

    commit

--- QuestionsFRomPlatforms/Leetcode/UglyNumber_263.java
+++ QuestionsFRomPlatforms/Leetcode/UglyNumber_263.java
@@ -4,5 +4,31 @@ public class UglyNumber_263 {
     public static void main(String[] args) {
         
     }
-    
+    public boolean isUgly(int n) {
+        // 2, 3, 5
+        if(n<=0) {
+            return false;
+        }
+        while(n%2==0){
+            n/=2;
+        }
+        while(n%3==0){
+            n/=3;
+        }
+        while(n%5==0){
+            n/=5;
+        }
+        return(n==1);
+    }
 }
+    
+
+
+
+/* 
+ * NOTE:
+ *  also called "regular numbers" or "Hamming numbers" = 2^a * 3^b * 5^c
+*/
+//https://leetcode.com/problems/ugly-number/
+
+
