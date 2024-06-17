e32a7e9e84c8d2c6c225ddcf0df1945fc69dc96b
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-06-17 13:01:34 +0530

    commit

---
+++ QuestionsFRomPlatforms/GFG/Gcd.java
@@ -0,0 +1,19 @@
+package QuestionsFromPlatforms.GFG;
+public class Gcd{
+    public static void main(String[] args) {
+        System.out.println(gcd(20,40));
+        
+    }
+    public static int gcd(int a, int b){
+        int temp = 0 ; 
+        while(b!=0){
+            b=temp;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+    }
+}
+
+
+// *  Euclidean algorithm 
---
+++ QuestionsFRomPlatforms/Leetcode/PowerOf2_231.java
@@ -0,0 +1,20 @@
+package QuestionsFromPlatforms.Leetcode;
+
+public class PowerOf2_231 {
+    public static void main(String[] args) {
+        System.out.println(powerOf2(27));
+    }
+    public static boolean powerOf2(int n){
+        if(n<=0){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        while(n%2==0){
+            n/=2;
+        }
+
+        return (n==1);
+    }
+}
