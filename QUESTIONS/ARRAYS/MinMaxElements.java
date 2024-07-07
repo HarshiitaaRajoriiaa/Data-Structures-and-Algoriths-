543e12f1f57e9c14001df9e1e429797435d34b41
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-07-07 16:03:36 +0530

    commit

---
+++ QUESTIONS/ARRAYS/MinMaxElements.java
@@ -0,0 +1,22 @@
+package QUESTIONS.ARRAYS;
+
+public class MinMaxElements {
+    public static void main(String[] args) {
+        
+    }
+    public Pair<Long, Long> getMinMax(int[] arr) {
+        long min = Long.MAX_VALUE;
+        long max = Long.MIN_VALUE;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        
+        return new Pair<>(min, max);
+    }
+}
---
+++ QUESTIONS/STRINGS/ToLowerCase.java
@@ -0,0 +1,11 @@
+package QUESTIONS.STRINGS;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+        
+    }
+    public String toLowerCase(String s) {
+        String s1 = s.toLowerCase();
+        return s1;
+    }
+}
