aa6279ee4a4e447710a6f67c42e0148ddf32e8d0
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-07-10 18:22:11 +0530

    commit

---
+++ QUESTIONS/ARRAYS/MaxCons1s.java
@@ -0,0 +1,22 @@
+package QUESTIONS.ARRAYS;
+//https://leetcode.com/problems/max-consecutive-ones/
+public class MaxCons1s {
+    public static void main(String[] args) {
+        
+    }
+    public  static int findMaxConsecutiveOnes(int[] nums) {
+        int no1 = 0; //track no. of ones
+        int max =-1;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                no1++;
+                max=Math.max(no1,max);
+            }
+            else{
+                no1 = 0;
+            } 
+        }
+        return max;
+    }
+}
---
+++ QUESTIONS/ARRAYS/SubArrayWithMaxSum.java
@@ -0,0 +1,43 @@
+package QUESTIONS.ARRAYS;
+import java.util.*;
+import java.util.ArrayList;
+//https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
+public class SubArrayWithMaxSum {
+    public static void main(String[] args) {
+        
+    }
+    static long maximumSumSubarray(int K, ArrayList<Integer> Arr,int N){
+        // code here
+        long max_sum = 0;
+        long curr_sum = 0;
+        
+        // Sum of the first subarray of size K
+        for (int i = 0; i < K; i++) { // [0, K-1]
+            curr_sum += Arr.get(i); // Add elements to curr_sum
+        }   // TC = O(K)
+        
+        max_sum = curr_sum; // Initialize max_sum with the sum of the first subarray
+        
+        // Sliding window approach
+        for (int start = 1, end = K; end < N; start++, end++) {  // TC = O(N-K)
+            curr_sum = curr_sum - Arr.get(start - 1) + Arr.get(end);
+            max_sum = Math.max(max_sum, curr_sum);
+        }
+        
+        return max_sum;
+    }
+
+}
+
+/*
+* BRUTE FORCE METHOD:
+ * no of sub array = N-K+1
+ * per sub array TC for have sum  = 0(K)
+ * overall TC = (N-K+1)*K == N*K + K^2 + K == N*K.
+  
+  
+ 
+
+ * DESIRED TC = 0(N)/ Sliding window Concept:
+ * curr_sum = curr_sum - Arr.get(start - 1) + Arr.get(end);
+ */
