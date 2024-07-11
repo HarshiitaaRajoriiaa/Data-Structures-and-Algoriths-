1b45153ac2dddf9987868df764732beb4ae68a36
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-07-11 14:23:36 +0530

    commit

---
+++ QUESTIONS/ARRAYS/RangeSumQuery.java
@@ -0,0 +1,8 @@
+package QUESTIONS.ARRAYS;
+//https://leetcode.com/problems/range-sum-query-immutable/description/
+public class RangeSumQuery {
+    public static void main(String[] args) {
+        
+    }
+    
+}
