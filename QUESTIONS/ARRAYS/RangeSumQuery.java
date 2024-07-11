ee7b516f50933f7e59de01ee7e9bb44be8494c56
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-07-11 15:07:54 +0530

    commit

--- QUESTIONS/ARRAYS/RangeSumQuery.java
+++ QUESTIONS/ARRAYS/RangeSumQuery.java
@@ -4,5 +4,9 @@ public class RangeSumQuery {
     public static void main(String[] args) {
         
     }
-    
+
 }
+/*
+ * int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+ * can be calculated like prefixSumArray[right-1] + arr[right] -arr[left-1]
+ */
