eaad75bf8926897cd685d29a1a37b4fd683e2b4c
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-07-14 11:51:25 +0530

    c

---
+++ QUESTIONS/ARRAYS/1D_ARRAYS/ProductOfArray_ExceptSelf.java
@@ -0,0 +1,39 @@
+//https://leetcode.com/problems/product-of-array-except-self/?envType=study-plan-v2&envId=leetcode-75
+public class ProductOfArray_ExceptSelf{
+    public static void main(String[] args) {
+        
+    }
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+        int[] result = new int[n];
+        
+        // Initialize the prefix and suffix arrays
+        prefix[0] = nums[0];
+        suffix[n - 1] = nums[n - 1];
+        
+        // Calculate the prefix products
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] * nums[i];
+        }
+        
+        // Calculate the suffix products
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] * nums[i];
+        }
+        
+        // Construct the result array
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                result[i] = suffix[i + 1];
+            } else if (i == n - 1) {
+                result[i] = prefix[i - 1];
+            } else {
+                result[i] = prefix[i - 1] * suffix[i + 1];
+            }
+        }
+        
+        return result;
+    }
+}
