fa1125346a049f1b6f562c40d7fb2c049b919404
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-07-14 14:21:52 +0530

    c

--- QUESTIONS/ARRAYS/1D_ARRAYS/ProductOfArray_ExceptSelf.java
+++ QUESTIONS/ARRAYS/1D_ARRAYS/ProductOfArray_ExceptSelf.java
@@ -3,6 +3,25 @@ public class ProductOfArray_ExceptSelf{
     public static void main(String[] args) {
         
     }
+    public int[] productExceptSelfOptimal(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
+
+
+         // Step 1: Calculate prefix products and store them in the output array
+        output[0] = 1;  // The first element has no left elements
+        for (int i = 1; i < n; i++) {
+            output[i] = output[i - 1] * nums[i - 1];
+        }
+        int suffixProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            output[i] *= suffixProduct;  // Multiply with the suffix product
+            suffixProduct *= nums[i];  // Update the suffix product
+        }
+        return output;
+    }
+
+
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] prefix = new int[n];
