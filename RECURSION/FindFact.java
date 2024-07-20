74a62634bf3f5dcf63f3e47583ed538aba96ae69
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-07-20 13:31:33 +0530

    a

--- RECURSION/FindFact.java
+++ RECURSION/FindFact.java
@@ -2,25 +2,12 @@ public class FindFact{
     public static void main(String[] args) {
         
     }
-    //without recursion:
-    public static void factorial1(int n){
-        int ans = 1;
-        for(int i = n; i > 0; i--){
-            ans = ans * i;
-        }
-        System.out.println(ans);
-    }
 
     //with recursion:
     public static int  factorial2(int n){
-        int result = 0 ;
-        if (n ==0 || n==1){
-            return 1;
-        }
 
-        else{
-            result  = n*factorial2(n-1);
-        }
+        if (n ==0 ) return 1;
+        int result  = n * factorial2(n-1);
         return result;
-        }
+    }
 }
