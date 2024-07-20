2c0ef228749ec690e0fc4c4ad2c6cfa84bb74798
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-07-20 16:42:50 +0530

    c

--- RECURSION/IsPalindrome.java
+++ RECURSION/IsPalindrome.java
@@ -1,23 +1,20 @@
 
 //https://www.geeksforgeeks.org/problems/palindrome-string0817/1
 public class IsPalindrome {
-    public static void main(String[] args) {
-
-        
+    public static void main(String[] args) {        
     }
-    public static int isPalindrome(String S) {
-        int left = 0;
-        int right = S.length() -1;
-        return palindrome(S, left, right) ? 1 : 0;
-    }
-    
-    public static boolean  palindrome(String S , int left, int right) {
-      if(left >=right){
-         return  true;
+    public int isPalindrome(String s) {
+        return isPalindromeHelper(s, 0, s.length() - 1);
       }
-      if (S.charAt(left) != S.charAt(right)) {
-            return false;
+      
+      private static int isPalindromeHelper(String s, int start, int end) {
+        if (start >= end) { // Base case: single character or empty string (palindrome)
+          return 1;
         }
-    return palindrome(S, left + 1, right - 1);  
-    }
+        if (s.charAt(start) != s.charAt(end)) { // Not a palindrome
+          return 0;
+        }
+        return isPalindromeHelper(s, start + 1, end - 1); // Recursively check remaining substring
+      }
+      
 }
