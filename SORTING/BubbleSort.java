8427dcfd23fd57cb830093db6629799db5a7c6f2
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-07-24 19:01:26 +0530

    c

--- SORTING/BubbleSort.java
+++ SORTING/BubbleSort.java
@@ -4,15 +4,19 @@ public class BubbleSort {
     public static void main(String[] args) {
         
     }
-    public static void BubbleSort(int arr[]){
-        int n = arr.length;
-        for(int i = 0 ; i<n;i++){
-            
+    public static void bubbleSort(int a[], int n)
+    {
+        for(int i = 0 ;i<n;i++){
+            // int j=0;
+            for(int j = 0 ;j <n-1;j++){
+                if(a[j] > a[j+1]){
+                    //swap
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+            // j++;
         }
-    }
-    public static void swap(int arr[], int i1 , int i2){
-        int temp= arr[i1];
-        arr[i2] = arr[i1];
-        arr[i1] = temp;
     }
 }
--- SORTING/SelectionSort.java
+++ SORTING/SelectionSort.java
@@ -2,28 +2,35 @@ package SORTING;
 
 public class SelectionSort {
     public static void main(String[] args) {
+        int arr[] = {12, 3, 9, 7, 8 , 1 , 2 ,  7 , 5 }; //
+        System.out.println(select(arr, 2));
         
     }
-    public static void selectionSort(int arr[]){
-        int i = 0;
-        while(i <arr.length){
-            
-        }
-    }
-    public static int findMinElementIndex(int arr[] , int st , int end){
-        int min = Integer.MAX_VALUE;
+    int  select(int arr[], int i){ //select minimum element of array from subarray [i ,n]{ 
+        // code here such that selectionSort() sorts arr[] , will select minimum element from [i to n-1]
+        int n = arr.length;
+        int min  = Integer.MAX_VALUE;
         int index = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i] <min){
-                min = arr[i];
-                index = i;
+        for(int j = i ; j< arr.length ; j++){
+            if(min > arr[j]){
+                min = arr[j];
+                index= j;
             }
         }
         return index;
-    }
-    public static void swap(int arr[], int i1 , int i2){
-        int temp= arr[i1];
-        arr[i1] = arr[i2];
-        arr[i2] = temp;
-    }
+	}
+	
+	void selectionSort(int arr[], int n)
+	{
+	    //code here
+	    for(int i = 0;i<n-1;i++){
+	        int index = select(arr , i);
+	        //swap
+	        int temp = arr[i];
+	        arr[i] = arr[index];
+	        arr[index] = temp;
+	    }
+	    
+	}
+    
 }
